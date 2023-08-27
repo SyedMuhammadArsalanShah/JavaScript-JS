6586d0711510925e25e44047b91ae8c9a451a85a
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2023-08-27 18:05:49 +0500

    lect 01 and 2

---
+++ Lecture01Variableandprint.js
@@ -0,0 +1,15 @@
+document.write("Bismillah <br>")
+console.log("hello world")
+
+
+
+var studentname ="Ayan";
+document.write("students name <br> "+studentname)
+
+
+
+var classname= "lab02 5 to 7<br> "
+document.write("Prog class name ="+classname)
+
+
+
---
+++ Lecture02datatype.js
@@ -0,0 +1,52 @@
+console.error("this is an  error")
+console.warn("this is an  error")
+
+
+
+// declare
+var names;
+// initialization
+names="christible"
+
+
+
+studentname = "Ayesha Amin"
+document.write(studentname)
+
+
+var classname = "javascript"
+document.write(classname)
+
+
+
+var age = 1.5
+document.write(age)
+
+
+var contact = 15
+document.write(contact)
+
+var ispubgfun = false
+
+document.write(ispubgfun)
+
+
+
+alert("aap ki jaan ko khatra hai")
+
+
+
+
+confirm("are you want to join our team")
+
+
+
+
+devname=prompt("Enter Your Name")
+
+
+
+document.write(devname)
+
+
+
